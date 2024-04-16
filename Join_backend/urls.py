0abc936d16fbfad3bd2d129c014ab009029b6470
Alexej Hein <alexej.hein@aol.de>
2024-04-16 10:01:48 +0200

    commit

--- Join_backend/urls.py
+++ Join_backend/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 from generator import views
 
@@ -7,5 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('generate-token/', views.generate_token),
     path('receive-data/', views.receive_data),
+    path('token-generator/', TemplateView.as_view(template_name='generate_token.html'), name='token-generator'),
+
 ]
 
